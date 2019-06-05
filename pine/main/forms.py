1a8d9d8bfdb2c0b66a22c355bfd4d9a9b3b66cf6
MayankPrasad99 <mayankpr.99@gmail.com>
2019-06-05 11:05:58 +0530

    added model forms (still need to add upload picture field )

---
+++ pine/main/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from .models import RentingUser
+
+
+class RentForm(forms.ModelForm):
+
+    class Meta:
+        model = RentingUser
+        fields = ('name', 'contact_number', 'number_of_rooms', 'locality', 'parking', 'balcony', 'individual',
+                  'attached_bathroom', 'kitchen', 'contact_time', 'prefered_customer', 'gender_pref', 'Paying_guest', 'people_per_room',
+                  'food',)
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Your Full Name'}),
+            'contact_number': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Contact Number'}),
+            'number_of_rooms': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Enter the Number of Rooms in the House'}),
+            'locality': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Locality'}),
+            'parking': forms.Select(attrs={'class':'regDropDown'}),
+            'balcony': forms.Select(attrs={'class':'regDropDown'}),
+            'individual': forms.Select(attrs={'class':'regDropDown'}),
+            'attached_bathroom': forms.Select(attrs={'class':'regDropDown'}),
+            'kitchen': forms.Select(attrs={'class':'regDropDown'}),
+            'contact_time': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Your Preferred Contact Time'}),
+            'prefered_customer': forms.Select(attrs={'class':'regDropDown'}),
+            'gender_pref': forms.Select(attrs={'class':'regDropDown'}),
+            'Paying_guest': forms.CheckboxInput(attrs={'class': 'form-control'}),
+            'people_per_room': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'how many Tenants per room (if PG)'}),
+            'food': forms.Select(attrs={'class':'regDropDown'}),
+        }
--- pine/main/models.py
+++ pine/main/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+y_n_choices = [
+    ('yes', 'Yes'),
+    ('no', 'No'),
+]
+
+
+class RentingUser(models.Model):
+    name = models.CharField(max_length=50)
+    contact_number = models.CharField(max_length=10)
+    number_of_rooms = models.IntegerField()
+    locality = models.CharField(max_length=100)
+    parking = models.CharField(max_length=3, choices=y_n_choices)
+    balcony = models.CharField(max_length=3, choices=y_n_choices)
+    individual = models.CharField(max_length=3, choices=y_n_choices)
+    attached_bathroom = models.CharField(max_length=3, choices=y_n_choices)
+    kitchen = models.CharField(max_length=15, choices=[('furnished', 'Furnished'), ('basic', 'Basic')])
+    contact_time = models.CharField(max_length=50)
+    prefered_customer = models.CharField(max_length=20)
+    gender_pref = models.CharField(max_length=10, choices=[('female', 'Female'), ('male', 'Male'), ('none', 'None')])
+    Paying_guest = models.BooleanField()
+    people_per_room = models.IntegerField()
+    food = models.CharField(max_length=3, choices=[('included', 'Included'), ('not included', 'Not Included')])
+
+    def __str__(self):
+        return self.name
+
+
