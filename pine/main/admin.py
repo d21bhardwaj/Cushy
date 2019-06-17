24ce88703d9abb2fe0f02d773177ad71f4f83a2d
MayankPrasad99 <mayankpr.99@gmail.com>
2019-06-17 14:06:43 +0530

    other 3 models registered

--- pine/main/admin.py
+++ pine/main/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import RentingUser
+from .models import RentingUser, RentingPGUser, Images, ImagesPG
 
 admin.site.register(RentingUser)
+admin.site.register(RentingPGUser)
+admin.site.register(Images)
+admin.site.register(ImagesPG)
+
