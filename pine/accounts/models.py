0a4b530a17974c9dd4d68ceea8a09e62e5c0b803
d21bhardwaj <39788819+d21bhardwaj@users.noreply.github.com>
2019-06-20 19:21:22 +0530

    profile view added, migrate once

--- pine/accounts/models.py
+++ pine/accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length = 181, blank=True)
+    mobile_no = models.IntegerField(null=True, blank=True, unique=True)
+    email = models.EmailField(max_length=70, null=True, blank=True, unique=True)
+    verified = models.BooleanField(default=False)
+    
+
+
+    def __str__(self):
+        return self.user.username
