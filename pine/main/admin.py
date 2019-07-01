56dbd8cdedbfb59fd8e802c017fc94673040fae0
d21bhardwaj <39788819+d21bhardwaj@users.noreply.github.com>
2019-07-02 00:22:48 +0530

    Update admin.py

--- pine/main/admin.py
+++ pine/main/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from .models import RentingUser, RentingPGUser, Images, ImagesPG
 
-admin.site.register(RentingUser)
-admin.site.register(RentingPGUser)
+class RentingUserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user_profile','updated_at','approved')
+
+class RentingPGUserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user_profile','updated_at','approved')
+
+admin.site.register(RentingUser, RentingUserAdmin)
+admin.site.register(RentingPGUser, RentingPGUserAdmin)
 admin.site.register(Images)
 admin.site.register(ImagesPG)
 
