2ab98738f8fbc2f81704a7734b3c98da04d36b3c
Ubuntu <ubuntu@ip-172-31-23-40.us-east-2.compute.internal>
2019-07-09 18:13:47 +0000

    changes

--- pine/pine/settings.py
+++ pine/pine/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'twhfs)&cu^-^8brtp+5-f3g&@-o#+a&876qk=i#6&lk$m^q#&d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.104','localhost','127.0.0.1']
+ALLOWED_HOSTS = ['192.168.1.104','localhost','127.0.0.1','*']
 
 
 # Application definition
