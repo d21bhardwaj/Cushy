efb358bd8f1d3127b61a4749237bc7376ab14544
Siddhant Sinha <35433185+sid573@users.noreply.github.com>
2019-07-11 00:04:55 +0530

    otp

--- pine/accounts/mobile_verification.py
+++ pine/accounts/mobile_verification.py
@@ -1,7 +1,7 @@
 import requests
 
 def otp_send(mobile):
-	url= "https://2factor.in/API/V1/9fd7dc0c-9a31-11e9-ade6-0200cd936042/SMS/+91"+str(mobile)+"/AUTOGEN"
+	url= "https://2factor.in/API/V1/9fd7dc0c-9a31-11e9-ade6-0200cd936042/SMS/+91"+str(mobile)+"/AUTOGEN/cushyroom"
 	response = requests.post(url)
 	#{ "Status": "Success", "Details": "5D6EBEE6-EC04-4776-846D"}
 	#print(response.json()['Details'])
