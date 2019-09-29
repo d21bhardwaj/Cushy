b29a720816fca6ff9d57aa87f4aba16fcdc7feb7
d21bhardwaj <39788819+d21bhardwaj@users.noreply.github.com>
2019-09-29 15:38:37 +0530

    Added Grocery app
    1) Apply migrations before continuing.

---
+++ pine/grocery/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Shop, Company, Category, Product
+
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ('id', 'shop')
+
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'company')
+    
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'category')
+    
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'company', 'category','price','product','quantity','shop','off','savings')
+
+admin.site.register(Shop, ShopAdmin)
+admin.site.register(Company, CompanyAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
+
+
---
+++ pine/grocery/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+# Create your models here.
+
+class Shop(models.Model):
+    shop = models.CharField(max_length=40, null = False, default='')
+    def __str__(self):
+        return str(self.shop)    
+
+class Company(models.Model):
+    company  = models.CharField(max_length=40, null = False, default='') 
+    # This class will be used for listing the product of the company
+    def __str__(self):
+        return str(self.company)
+    
+class Category(models.Model):
+    category = models.CharField(max_length=40, null = False, default='')
+    # This class will be used for listing products in sub category
+    def __str__(self):
+        return str(self.category)
+
+class Product(models.Model):
+    # This class will be used for listing the product company so on and so forth
+    product = models.CharField(max_length=40, null = False, default='')
+    quantity = models.CharField(max_length=10, null = False, default='')
+    company = models.ForeignKey(Company, on_delete=models.DO_NOTHING, null = False )
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING, null = False)
+    price = models.IntegerField(null = False, default='')
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE, null = False )
+    off = models.BooleanField(default=False)
+    savings = models.IntegerField(null = True)
