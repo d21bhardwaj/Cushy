420de57b3e58af05ef8b8859d9c3c7438020d2b0
Aryan Agrawal <32397411+aryan2305@users.noreply.github.com>
2019-10-24 20:49:01 +0530

    Update forms.py

--- pine/main/forms.py
+++ pine/main/forms.py
@@ -195,14 +195,46 @@ class ImageFormPG(forms.ModelForm):
         model = ImagesPG
         fields = ('image', )
 
-class FilterForm(forms.ModelForm):
+class FilterFormLocation(forms.ModelForm):
     LOCATION = ((x.id, x) for x in Location.objects.all())
+    print(LOCATION)
     Locations = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple,
                                           choices=LOCATION)
-    
     class Meta: 
-        model = Location 
+        model = Location
         fields = ('Locations',)
+
+class FilterFormPrice(forms.ModelForm):
+    PRICE = (
+        (0,'Less than 2000'),
+        (2000,'2000-4000'),
+        (4000,'4000-6000'),
+        (6000,'6000-8000'),
+        (8000,'8000-10000'),
+        (10000,'Above 10000')
+    )
+    Prices = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple,
+                                          choices=PRICE)
+    class Meta: 
+        model = RentingUser 
+        fields = ('Prices',)
+
+                    
+
+class FilterFormPGPrice(forms.ModelForm):
+    PRICE = (
+        (0,'Less than 2000'),
+        (2000,'2000-4000'),
+        (4000,'4000-6000'),
+        (6000,'6000-8000'),
+        (8000,'8000-10000'),
+        (10000,'Above 10000')
+    )
+    Prices = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple,
+                                          choices=PRICE)
+    class Meta: 
+        model = RentingPGUser 
+        fields = ('Prices',)     
 
 class ContactForm(forms.Form):
     contact_name = forms.CharField(required=True)
