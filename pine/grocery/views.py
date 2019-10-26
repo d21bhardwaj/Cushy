869cf1e2ecdec492380054a55d4c82a1d5e75305
d21bhardwaj <bhard170121014@iitg.ac.in>
2019-10-26 19:30:44 +0530

    Mionor changes in grocery.view

--- pine/grocery/views.py
+++ pine/grocery/views.py
@@ -4,12 +4,8 @@ from .models import Product
 from accounts.models import Profile
 from django.core.exceptions import PermissionDenied
 
-
 #adding for contact form
-from django.core.mail import EmailMessage
-from django.shortcuts import redirect
-from django.template.loader import get_template
-from django.contrib.auth.decorators import login_required, user_passes_test
+
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.core.exceptions import ObjectDoesNotExist
@@ -18,58 +14,33 @@ from .models import Product,Company,Shop,Category
 
 import openpyxl as xl
 
-def data_upload(request):
-    if request.method == "POST":
-        excel_file = request.FILES["excel_file"]
-        wb = xl.load_workbook(excel_file)
-        sheet = wb.active
-        # print(sheet)
-        max_r = sheet.max_row
-# from django.shortcuts import render, redirect
-# from django.contrib.auth import logout
-# from django.forms import modelformset_factory
-#
-# #for profile linking
-# from accounts.models import Profile
-# from django.core.exceptions import PermissionDenied
-#
-# #adding for contact form
-# from django.core.mail import EmailMessage
-# from django.shortcuts import redirect
-# from django.template.loader import get_template
-# from django.contrib.auth.decorators import login_required, user_passes_test
-# from django.contrib.auth.models import User
-# from django.contrib import messages
-# from django.core.exceptions import ObjectDoesNotExist
-# from django.http import FileResponse
 from .models import Product,Company,Shop,Category
-import xlrd as xl
+
 from django.shortcuts import render
 from django.http import Http404
+from django.http import HttpResponse
 import json
 from django.conf import settings
 
 def cart_add(request):
     user_id = request.user.pk
     product_id = request.POST.get('product_id')
+    # quantity = request.POST.get('quantity')
     quantity = 2
-    new_item = []
-    new_item.append({str(product_id):quantity})
     file_path = settings.BASE_DIR + '/static/json/user' + str(user_id) + 'cart.json'
     try:
-        with open(file_path, 'r+') as json_read:
+        with open(file_path, 'r') as json_read:
             data = json.loads(json_read.read())
-        data[str(user_id)].extend(add)
+        data[str(product_id)] = quantity
         with open(file_path, 'w+') as f:
             json.dump(data, f)
     except:
         with open(file_path, 'w+') as json_file:
-            d = {}
-            quantity = 3;
-            d[str(product_id)] = 3
-            json.dump(d, json_file)
+            add = {}
+            quantity = 3
+            add[str(product_id)] = quantity
+            json.dump(add, json_file)
     return render(request, 'groceries.html')
-
 
 def show_savings(cart):
     total_savings = 0
@@ -107,13 +78,13 @@ def cart_empty(request):
 
 
 
-
 def data_upload(request):
     if request.method == "POST":
-
         excel_file = request.FILES["excel_file"]
-        wb = xl.open_workbook(excel_file)
-        sheet = wb.sheet_by_index(0)
+        wb = xl.load_workbook(excel_file)
+        sheet = wb.active
+        # print(sheet)
+        max_r = sheet.max_row
         dic = []
         for i in range(1,max_r+1): 
 
@@ -133,7 +104,7 @@ def data_upload(request):
                 ))
 
         Product.objects.bulk_create(dic)
-        return HttpResponse("Data created at server for"+" "+str(token))
+        return HttpResponse("Data created at server for"+" ")
 
     else:
 	    return render(request,"upload_data.html")
